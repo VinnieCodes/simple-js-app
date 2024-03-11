0b5f0ef9b3c38ee8d1e1a85dfb34cede4eea05ca
Vinnie <vincentathorne2005@gmail.com>
2024-03-11 13:32:07 -0700

    adding different buttons via DOM

--- js/script.js
+++ js/script.js
@@ -1,95 +1,47 @@
-(function () {
-  let pokemonList = [
+let pokemonRepository = (function () {
+  let repository = [
     { name: "Bulbasaur", height: ".7", type: ["grass", "poison"] },
     { name: "Blastoise", height: "1.6", type: "water" },
     { name: "Ditto", height: ".3", type: "normal" },
     { name: "Rattata", height: ".3", type: "normal" },
   ];
-  // displays pokemons and their heights
-  pokemonList.forEach(function (pokmon) {
-    if (pokmon.height >= 1) {
-      document.write(
-        pokmon.name +
-          " (height: " +
-          pokmon.height +
-          "m) - " +
-          "that's a big one" +
-          "</br></br>"
-      );
-    } else if (0.5 < pokmon.height && pokmon.height < 1) {
-      document.write(
-        pokmon.name +
-          " (height: " +
-          pokmon.height +
-          "m) - " +
-          "that's medium" +
-          "</br></br>"
-      );
-    } else {
-      document.write(
-        pokmon.name +
-          " (height: " +
-          pokmon.height +
-          "m) - " +
-          "that's small" +
-          "</br></br>"
-      );
-    }
-  });
-})();
-
-// can see the pokemon list and add pokemons
-let pokemonRepository = (function () {
-  let pokemonList = [];
 
   function add(pokemon) {
-    pokemonList.push(pokemon);
+    if (
+      typeof pokemon === "object" &&
+      "name" in pokemon &&
+      "height" in pokemon &&
+      "types" in pokemon
+    ) {
+      repository.push(pokemon);
+    } else {
+      console.log("pokemon is not correct");
+    }
   }
-
   function getAll() {
-    return pokemonList;
+    return repository;
   }
-
+  function addListItem(pokemon) {
+    let pokemonList = document.querySelector(".pokemon-list");
+    let listpokemon = document.createElement("li");
+    let button = document.createElement("button");
+    button.innerText = pokemon.name;
+    button.classList.add("btn-class");
+    listpokemon.appendChild(button);
+    pokemonList.appendChild(listpokemon);
+  }
   return {
     add: add,
     getAll: getAll,
+    addListItem: addListItem,
   };
 })();
 
-console.log(pokemonRepository.getAll()); // []
-pokemonRepository.add({ name: 'Pikachu' });
-console.log(pokemonRepository.getAll()); // [ { name: 'Pikachu' } ]
-// displays pokemons and their heights
-// for (i = 0; i < pokemonList.length; i++) {
-//   let pokemonHeight = pokemonList[i].height;
-//   let pokemonName = pokemonList[i].name;
+pokemonRepository.add({ name: "Pikachu", height: 0.3, types: ["electric"] });
 
-//   if (pokemonList[i].height >= 1) {
-//     document.write(
-//       pokemonList[i].name +
-//         " (height: " +
-//         pokemonList[i].height +
-//         "m) - " +
-//         "that's a big one" +
-//         "</br></br>"
-//     );
-//   } else if (0.5 < pokemonList[i].height && pokemonList[i].height < 1) {
-//     document.write(
-//       pokemonList[i].name +
-//         " (height: " +
-//         pokemonList[i].height +
-//         "m) - " +
-//         "that's medium" +
-//         "</br></br>"
-//     );
-//   } else {
-//     document.write(
-//       pokemonList[i].name +
-//         " (height: " +
-//         pokemonList[i].height +
-//         "m) - " +
-//         "that's small" +
-//         "</br></br>"
-//     );
-//   }
-// }
+console.log(pokemonRepository.getAll());
+
+pokemonRepository.getAll().forEach(function (pokemon) {
+  pokemonRepository.addListItem(pokemon);
+});
+
